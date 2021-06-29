71d586ae4c2fa0c14beba7ff86c99301d6dffa63
B. Klara Simon <borbala.klara@gmail.com>
2021-06-29 23:37:23 +0200

    add function

---
+++ script.js
@@ -0,0 +1,23 @@
+function dragStart() {
+  console.log("drag start");
+}
+
+function dragEnd() {
+  console.log("drag end");
+}
+
+function dragOver() {
+  console.log("drag start");
+}
+
+function dragEnter() {
+  console.log("drag start");
+}
+
+function dragLeave() {
+  console.log("drag start");
+}
+
+function dragDrop() {
+  console.log("drag start");
+}
